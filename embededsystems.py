a40b96a8be52b4063f68e4ee83e960b7a2a27fa7
ipaschalis <ilias2002vip.gr@gmail.com>
2023-05-03 13:39:28 +0300

    debug

--- embededsystems.py
+++ embededsystems.py
@@ -1,10 +1,15 @@
 # Paschalis Ilias
+# ipaschalis2002@gmail.com
+# Co programers:
+# Ralousis Anastasios
+# Giarmantzis Ephstratios
 # CircuitPython
 # Line follower
 
 import board
 import digitalio
 import simpleio
+import neopixel
 import time
 import pwmio
 from adafruit_motor import motor
@@ -24,34 +29,52 @@ motorR = motor.DCMotor(mR1, mR2)
 right_ir = digitalio.DigitalInOut(board.GP4)
 left_ir = digitalio.DigitalInOut(board.GP6)
 
-# Alazei thn taxitita twn kinitirwn
-# Dehete input apo 0 eos 1, px 0.5 . 
+# Initialize Neopixel RGB LEDs
+pixels = neopixel.NeoPixel(board.GP18, 2)
+pixels.fill(0)
+color = 0
+state = 0
+
+# Αλλάζει την ταχύτητα των κινητήρων
+# Δέχεται από -1 έως 1, πχ 0.5
 def moveM(LThro, RThro):
     motorL.throttle = -LThro
     motorR.throttle = RThro
 
 def mForward():
-    moveM(0.25, 0.25)
-
+    moveM(0.30, 0.30)
+    
+def mBack():
+    moveM(-0.30, -0.30)
+    
 def mBreak():
     moveM(0, 0)
     
 def turnRight():
-    moveM(0, 0.15)
+    moveM(0, 0.25)
     
 def turnLeft():
-    moveM(0.15, 0)
-
+    moveM(0.25, 0)
+    
 if __name__ == "__main__":
     mBreak()
+    time.sleep(1)
+
     while True:
-        if not right_ir.value and not left_ir.value:
+        time.sleep(0.1)
+        r_ir = right_ir.value
+        l_ir = left_ir.value
+        
+        if not r_ir and not l_ir: # αν δεν βλέπει την γραμμή ξεκινάει
             mForward()
-        elif right_ir.value and left_ir.value:
+        elif r_ir and l_ir: # αν βλέπει την γραμμή και από τους δυο αισθητήρες κάνει πίσω και σταματάει   
+            mBack()
+            time.sleep(0.2)
             mBreak()
-        elif not right_ir.value and left_ir.value:
+            time.sleep(1)
+        elif not r_ir and l_ir: # αν βλέπει την γραμμή από έναν αισθητήρα γυρνάει  
             turnRight()
-        elif right_ir.value and not left_ir.value:
+        elif r_ir and not l_ir:
             turnLeft()
         else:
             mBreak()
