ad2c18c634532467591768b16f3f2c7dea641676
ipaschalis <ilias2002vip.gr@gmail.com>
2023-05-03 12:24:26 +0300

    Update embededsystems.py

--- embededsystems.py
+++ embededsystems.py
@@ -7,7 +7,7 @@ import digitalio
 import simpleio
 import time
 import pwmio
-from adafruit_motor import servo, motor
+from adafruit_motor import motor
 
 # initialize DC motors
 freq = 10000
@@ -20,26 +20,38 @@ mR1 = pwmio.PWMOut(board.GP10, frequency = freq)
 mR2 = pwmio.PWMOut(board.GP11, frequency = freq)
 motorR = motor.DCMotor(mR1, mR2)
 
+# initialize IR sensors
+right_ir = digitalio.DigitalInOut(board.GP4)
+left_ir = digitalio.DigitalInOut(board.GP6)
+
 # Alazei thn taxitita twn kinitirwn
 # Dehete input apo 0 eos 1, px 0.5 . 
 def moveM(LThro, RThro):
-    motorL.throttle = LThro
-    motorR.throttle = -RThro
+    motorL.throttle = -LThro
+    motorR.throttle = RThro
 
 def mForward():
-    moveM(0.5, 0.5)
+    moveM(0.25, 0.25)
 
 def mBreak():
     moveM(0, 0)
     
 def turnRight():
-    moveM(0, 0.25)
+    moveM(0, 0.15)
     
 def turnLeft():
-    moveM(0.25, 0)
+    moveM(0.15, 0)
 
-while True:
-    mForward()
-    time.sleep(1)
+if __name__ == "__main__":
     mBreak()
-    time.sleep(0)
+    while True:
+        if not right_ir.value and not left_ir.value:
+            mForward()
+        elif right_ir.value and left_ir.value:
+            mBreak()
+        elif not right_ir.value and left_ir.value:
+            turnRight()
+        elif right_ir.value and not left_ir.value:
+            turnLeft()
+        else:
+            mBreak()
