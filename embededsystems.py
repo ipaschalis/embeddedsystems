2d4bfb1748fef7fde6fc4307bd72f20b81d34f86
ipaschalis <ilias2002vip.gr@gmail.com>
2023-05-28 11:06:26 +0300

    beautify

--- embededsystems.py
+++ embededsystems.py
@@ -1,5 +1,5 @@
 # Line follower
-# 	Version 1.0.2 Mar 5 2023
+# 	Version 1.0.4 Mar 28 2023
 # For Maker Pi RP2040 running CircuitPython
 
 # MIT license
@@ -13,7 +13,6 @@
 
 import board
 import digitalio
-import simpleio
 import time
 import pwmio
 from adafruit_motor import motor
@@ -21,12 +20,12 @@ from adafruit_motor import motor
 # initialize DC motors
 freq = 10000
 
-mL1 = pwmio.PWMOut(board.GP8, frequency = freq)
-mL2 = pwmio.PWMOut(board.GP9, frequency = freq)
+mL1 = pwmio.PWMOut(board.GP8, frequency=freq)
+mL2 = pwmio.PWMOut(board.GP9, frequency=freq)
 motorL = motor.DCMotor(mL1, mL2)
 
-mR1 = pwmio.PWMOut(board.GP10, frequency = freq)
-mR2 = pwmio.PWMOut(board.GP11, frequency = freq)
+mR1 = pwmio.PWMOut(board.GP10, frequency=freq)
+mR2 = pwmio.PWMOut(board.GP11, frequency=freq)
 motorR = motor.DCMotor(mR1, mR2)
 
 # initialize IR sensors
@@ -36,61 +35,59 @@ left_ir = digitalio.DigitalInOut(board.GP6)
 
 # Αλλάζει την ταχύτητα των κινητήρων
 # Δέχεται από -1 έως 1, πχ 0.5
-def moveM(LThro, RThro):
-    motorL.throttle = -LThro
-    motorR.throttle = RThro
+def move_m(left_throttle, right_throttle):
+    motorL.throttle = -left_throttle
+    motorR.throttle = right_throttle
 
-def mForward():
-    moveM(0.30, 0.30)
-    
-def mBack():
-    moveM(-0.25, -0.25)
-      
-def mBreak():
-    moveM(0, 0)
-    
-def turnRight():
-    mBack()
+
+def move_forward():
+    move_m(0.30, 0.30)
+
+
+def move_back():
+    move_m(-0.25, -0.25)
+
+
+def move_stop():
+    move_m(0, 0)
+
+
+def turn_right():
+    move_back()
     time.sleep(0.25)
-    moveM(0, 0.30)
+    move_m(0, 0.30)
     time.sleep(0.5)
-    mBreak()
+    move_stop()
     time.sleep(0.1)
-    
-def turnLeft():
-    mBack()
+
+
+def turn_left():
+    move_back()
     time.sleep(0.25)
-    moveM(0.30, 0)
+    move_m(0.30, 0)
     time.sleep(0.5)
-    mBreak()
+    move_stop()
     time.sleep(0.1)
 
 
 if __name__ == "__main__":
-    mBreak()
-    time.sleep(1)
+    move_stop()
 
     while True:
         # Παύση για πιο ομαλή λειτουργιά
-        time.sleep(0.005) 
-        
+        time.sleep(0.005)
+
         # Διάβασμα αισθητήρων 
         r_ir = right_ir.value
         l_ir = left_ir.value
-        
-        if not r_ir and not l_ir: # αν δεν βλέπει την γραμμή ξεκινάει
-            mForward()
-            #time.sleep(0.1)
-            #mBreak()
-            
-        elif r_ir and l_ir: # αν βλέπει την γραμμή και από τους δυο αισθητήρες κάνει πίσω και σταματάει   
-            mBack()
-            time.sleep(0.1)
-            mBreak()
-            time.sleep(0.05)
-        elif not r_ir and l_ir: # αν βλέπει την γραμμή από έναν αισθητήρα γυρνάει  
-            turnRight()
+
+        if not r_ir and not l_ir:  # αν δεν βλέπει την γραμμή ξεκινάει
+            move_forward()
+        elif r_ir and l_ir:  # αν βλέπει την γραμμή και από τους δυο αισθητήρες κάνει πίσω και σταματάει
+            move_stop()
+        elif not r_ir and l_ir:  # αν βλέπει την γραμμή από έναν αισθητήρα γυρνάει
+            turn_right()
         elif r_ir and not l_ir:
-            turnLeft()
+            turn_left()
         else:
-            mBreak()
+            move_stop()
